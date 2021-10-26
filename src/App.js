f336cbf3405ebabd9067a10d86d544c74e78be9b
kmnsoo <wkdska22@naver.com>
2021-10-26 16:13:01 +0900

    Board component 추가, 내 State, 상대(other)state 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Button from "./Button";
-import Dice from "./Dice";
+import Board from "./Board";
 
 
 function random(n) {
@@ -12,36 +12,46 @@ function App() {
     const [sum, setSum] = useState(0);
     const[gameHistory, setGameHistory] = useState([]);
 
+    const [othernum, setOtherNum] = useState(1);
+    const [othersum, setOtherSum] = useState(0);
+    const[othergameHistory, setOtherGameHistory] = useState([]);
+
     const handleRollClick = () => {
         const nextNum = random(6);
+        const nextOtherNum = random(6);
+
         setNum(nextNum);
         setSum(sum + nextNum);
         setGameHistory([...gameHistory, nextNum]);
+
+        setOtherNum(nextOtherNum);
+        setOtherSum(othersum + nextOtherNum);
+        setOtherGameHistory([...othergameHistory, nextOtherNum]);
     };
 
     const handleClearClick = () => {
         setNum(1);
         setSum(0);
         setGameHistory([]);
+
+        setOtherNum(1);
+        setOtherSum(0);
+        setOtherGameHistory([]);
     };
 
-    return (
-    <div>
-     <div>
-      <Button onClick = {handleRollClick}>던지기</Button>
-      <Button onClick = {handleClearClick}>처음부터</Button>
-    </div>
-    <div>
-        <h2>나</h2>
-        <Dice color = "blue" num ={num} />
-        <h2>총점</h2>
-        <p>{sum}</p>
-        <h2>기록</h2>
-        <p>{gameHistory.join(', ')}</p>
-    </div>
-    </div>
+    return(
+        <div>
+             <div>
+                <Button onClick = {handleRollClick}>던지기</Button>
+                <Button onClick = {handleClearClick}>처음부터</Button>
+             </div>
+             <div>
+                <Board name= "나" color ="blue" num ={num} sum = {sum} gameHistory = {gameHistory}/>
+                <Board name = "상대" color ="red" num ={othernum} sum = {othersum} gameHistory = {othergameHistory}/>
+            </div>
+         </div>
+        
     );
 }
 
 export default App;
-
