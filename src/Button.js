b9c463461dcde9037d2bfc2f61a82ed2f7dda1c9
kmnsoo <wkdska22@naver.com>
2021-10-27 10:28:26 +0900

    build 완료

--- src/Button.js
+++ src/Button.js
@@ -1,12 +1,13 @@
 import './Button.css';
 
 function Button({ className = '', color = 'blue', children, onClick }) {
+
   const classNames = `Button ${color} ${className}`;
+
   return (
     <button className={classNames} onClick={onClick}>
       {children}
     </button>
   );
 }
-
 export default Button;
